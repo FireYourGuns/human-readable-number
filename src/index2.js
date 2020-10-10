7756403035e8082c7744cafaf6befabaf410fac0
FireYourGuns <chazovby@gmail.com>
2020-10-10 18:17:47 +0300

    refactor: add new files

---
+++ src/index2.js
@@ -0,0 +1,37 @@
+    var keywords = {
+            "0":"","1":"one","2":"two","3":"three","4":"four","5":"five","6":"six","7":"seven","8":"eight","9":"nine","10":"ten",
+            "11":"eleven","12":"twelve","13":"thirteen","14":"fourteen","15":"fifteen","16":"sixteen","17":"seventeen","18":"eighteen","19":"nineteen",
+            "20":"twenty","30":"thirty","40":"forty","50":"fifty","60":"sixty","70":"seventy","80":"eighty"
+            ,"90":"ninety"
+              }
+    
+function toReadable(number) {
+    var Mas = number.toString().split('');
+          var str = "";
+      
+          if (Mas.length === 3) {
+            if (+(Mas[1]+Mas[2]+"") < 20) {
+                str = keywords[Mas[0]] + " hundred " + keywords[+(Mas[1]+Mas[2]+"")]
+            } else if (Mas[2]==0) {
+              str = keywords[Mas[0]] + " hundred " + keywords[(+Mas[1]*10)]
+            } else {
+            str = keywords[Mas[0]] + " hundred " + keywords[(+Mas[1]*10)] + " " + keywords[Mas[2]]
+            }
+        };
+
+        if (Mas.length === 2) {
+          if (number < 20 || Mas[1] == 0) {
+              str = keywords[number]
+          } else {
+              str = keywords[(+Mas[0]*10)] + " " + keywords[Mas[1]]
+          }
+      }; 
+
+        if (Mas.length === 1) {
+            str = keywords[Mas[0]]
+        };
+      
+      return str;
+}
+    console.log(toReadable(909))
+  
